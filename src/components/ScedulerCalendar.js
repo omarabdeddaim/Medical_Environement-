43f75fb82308cb22524357f851dde2b408e05dd5
omarabdeddaim <50836971+omarabdeddaim@users.noreply.github.com>
2019-12-20 12:33:43 +0100

    Update ScedulerCalendar.js

--- src/components/ScedulerCalendar.js
+++ src/components/ScedulerCalendar.js
@@ -5,7 +5,12 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 export default class ScedulerCalendar extends Component {
   render() {
     return (
-      <FullCalendar defaultView="dayGridMonth" plugins={[dayGridPlugin]} />
+      <FullCalendar defaultView="dayGridMonth" plugins={[dayGridPlugin]} 
+      events={[
+        { title: 'event 1', date: '2019-04-01' },
+        { title: 'event 2', date: '2019-04-02' }
+      ]}
+      />
     );
   }
 }
