bd6fd7729859be4e748aef68e4f291ec51da5c23
omarabdeddaim <50836971+omarabdeddaim@users.noreply.github.com>
2019-12-25 00:32:34 +0100

    25.12.2019

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./App.css";
-import ScedulerCalendar from "./components/ScedulerCalendar";
-function App() {
-  return <ScedulerCalendar />;
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,100 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { render } from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import FullCalendar from "sardius-fullcalendar-wrapper";
+import interactionPlugin from "@fullcalendar/interaction";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import "@fullcalendar/core/main.css";
+import "@fullcalendar/daygrid/main.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const today = new Date();
+const tomorrow = new Date();
+
+class ExampleComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    // Create a reference to the component to use Full Calendar methods
+    this.calendarApiRef = React.createRef();
+    this.state = {
+      events: [
+        {
+          title: "Example Event",
+          start: today
+        },
+        {
+          title: "Example Event",
+          start: tomorrow.setDate(today.getDate() + 1)
+        }
+      ]
+    };
+  }
+
+  eventClicked = eventClickInfo => {
+    <>
+      <html>
+        <head>veuillez vous s'il te plait ajouter un nouveller evenement</head>
+        <body>
+          <form>
+            <li>nome</li>
+            <textarea> </textarea>
+          </form>
+        </body>
+      </html>
+    </>;
+  };
+
+  getView = () => {
+    // Use reference to call Full Calendar Methods
+    const view = this.calendarApiRef.current.calendar.getView();
+    alert("We are using FullCalendar Methods!");
+  };
+
+  selectEvent = selectionInfo => {
+    alert(
+      <>
+        <html>
+          <head>
+            veuillez vous s'il te plait ajouter un nouveller evenement
+          </head>
+          <body>
+            <form>
+              <li>nome</li>
+              <textarea> .......</textarea>
+            </form>
+          </body>
+        </html>
+      </>
+    );
+  };
+
+  render() {
+    return (
+      <FullCalendar
+        ref={this.calendarApiRef}
+        nowIndicator
+        header={{
+          left: "prev,today,next",
+          center: "title",
+          right: "dayGridMonth, dayGridWeek, dayGridDay"
+        }}
+        navLinks
+        events={this.state.events}
+        select={selectionInfo => {
+          this.selectEvent(selectionInfo);
+        }}
+        // Another example of a callback / handler function
+        eventClick={eventClickInfo => {
+          this.eventClicked(eventClickInfo);
+        }}
+        plugins={[interactionPlugin, dayGridPlugin]}
+        editable
+        selectable
+        snapDuration="00:05"
+        allDaySlot={false}
+        defaultView="dayGridMonth"
+      />
+    );
+  }
+}
+
+render(<ExampleComponent />, document.getElementById("root"));
